a01fc6d00d40ae1b682ea562f77a3ddcf8dbe555
Blue88Comanche <jeffreyenfinger@yahoo.com>
2013-02-20 14:53:36 -0800

    Added Deposit form

--- Forms.py
+++ Forms.py
@@ -1,4 +1,4 @@
-print 'Form Finder V1.0.2'
+print 'Form Finder V1.0.3'
 #
 import os
 #
@@ -9,11 +9,14 @@ print '1 = Tanning Agreement'
 print '2 = Key Usage'
 print '3 = Equipment and Facility'
 print '4 = Safty Notices'
+print
 print '5 = Guest Regeister'
 print '6 = Guest Pass'
+print
 print '7 = Cancelation Form'
 print '8 = Fax Cover Sheet'
 print '9 = General cleaning check list'
+print '10 = Deposit Receipt'
 print
 print
 print '20 = Everything for a new member'
@@ -37,6 +40,8 @@ if form is 8:
     os.startfile("forms\Fax Cover Sheet.docx")
 if form is 9:
     os.srartfile("forms\General cleaning check list.docx")
+if form is 10:
+    os.srartfile("forms\Deposit Receipt.docx")
 if form is 20:
     os.startfile("forms\TANNING AGREEMENT.docx")
     os.startfile("forms\Important Notice Regarding Key Usage.doc")
