0e56288ef7f5e2ef0a04e2d5243ae1402844010e
Blue88Comanche <jeffreyenfinger@yahoo.com>
2013-11-01 10:01:42 -0700

    moved rates to a separate file to clean up program

--- MemberCal.py
+++ MemberCal.py
@@ -1,6 +1,6 @@
 #this program is not for resale
 #Membership Calculator written By Jeffrey Enfinger
-print "membership Calculator v3.0.3"
+print "membership Calculator v3.0.4"
 print
 import os
 ######
@@ -25,90 +25,8 @@ start_billing = str(next_month) + '/' + str(current_day) + '/' + str(next_year)
 correct = ['yes', 'Yes', 'YEs', 'YES', 'yES', 'yeS', 'y', 'Y', '1',];
 discount_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 31, 32, 35,];
 while True:
-	##########################################
-	## Membership Info						##
-	## Edit this to change membership rates ##
-	##########################################
-	# Primary member rate
-	single_per = 35
-	# Secondary member rate
-	extra = 20
-	# Primary start up fee
-	startup_1 = 15
-	# Secondary start up fee
-	startup_extra = 5
-	# Tanning rate # not used
-	tanning = 0
-	# Anytume Health fee # not used
-	anytime_health = 0
-	#######
-	#Discount Field
-	#Don't change this field
-	#######
-	#zero Start up
-	zero_start = startup_1 - startup_1
-	zero_start_extra = startup_extra - startup_extra
-	#10% off monthly
-	ten_off = single_per * .9
-	ten_off_extra = extra * .9
-	#City of mobile
-	city_start = startup_1 * .5
-	city_per = single_per * .9
-	#Mobile County
-	county_start = startup_1 * .5
-	county_per = single_per * .9
-	#State of Alabama
-	state_start = startup_1 * .5
-	state_per = single_per * .9
-	#School Teachers
-	teacher_start = startup_1 * .5
-	teacher_per = single_per * .9
-	#AMR (American Medical Responce)
-	amr_start = startup_1 * .5
-	amr_per = single_per * .9
-	#Wal*mart or Sam's Club
-	wal_or_sam_start = startup_1 * .5
-	wal_or_sam_per = single_per * .9
-	wal_or_sam_tan = tanning * .9
-	#Target Corporation
-	target_start = startup_1 * .5
-	target_per = single_per * .9
-	target_tan = tanning * .9
-	#Walgreens
-	walgreens_start = startup_1 * .5
-	walgreens_per = single_per * .9
-	#Inner Strength Martial Arts
-	inner_strength_per = single_per * .9
-	#ClearWater Christian Church
-	clearwater_per = single_per * .9
-	#Military
-	mil_per = single_per * .9
-	mil_extra = extra * .9
-	mil_start = startup_1 * .5
-	mil_extra_start = startup_extra * .5
-	#silver sneakers
-	silver = single_per * 0
-	silver_start = startup_1 * 0
-	#Home Depot
-	home_depot_per = single_per * .9
-	home_depot_start = startup_1 * .5
-	#Pepsi
-	pepsi_per = single_per * .9
-	pepsi_start = startup_1 * .5
-	#Student
-	student = single_per * .9
-	student_fee = startup_1 * .5
-	#Coca-Cola
-	coca_cola = single_per * .9
-	coca_cola_fee = startup_1 * .5
-	#Delta
-	delta = single_per * .9
-	#Kohl's
-	kohls = single_per * .9
-	kohls_start = startup_1 * .5
-	#Unitedhealth Allies
-	unitedhealth = single_per * .9
-	unitedhealth_start = startup_1 * .5
+	import rates
+	from rates import *		   
 	###############################################################
 	## Start of Program ## Don't change anything past this point ##
 	print "How long is the membership? (In months, up to 12)"
@@ -397,3 +315,4 @@ while True:
 	else:
 		os.system('CLS')
 	##### end #####
+	reload(rates)
---
+++ rates.py
@@ -0,0 +1,84 @@
+##########################################
+## Membership Info						##
+## Edit this to change membership rates ##
+##########################################
+# Primary member rate
+single_per = 35
+# Secondary member rate
+extra = 20
+# Primary start up fee
+startup_1 = 15
+# Secondary start up fee
+startup_extra = 5
+# Tanning rate # not used
+tanning = 0
+# Anytume Health fee # not used
+anytime_health = 0
+#######
+#Discount Field
+#Don't change this field
+#######
+#zero Start up
+zero_start = startup_1 - startup_1
+zero_start_extra = startup_extra - startup_extra
+#10% off monthly
+ten_off = single_per * .9
+ten_off_extra = extra * .9
+#City of mobile
+city_start = startup_1 * .5
+city_per = single_per * .9
+#Mobile County
+county_start = startup_1 * .5
+county_per = single_per * .9
+#State of Alabama
+state_start = startup_1 * .5
+state_per = single_per * .9
+#School Teachers
+teacher_start = startup_1 * .5
+teacher_per = single_per * .9
+#AMR (American Medical Responce)
+amr_start = startup_1 * .5
+amr_per = single_per * .9
+#Wal*mart or Sam's Club
+wal_or_sam_start = startup_1 * .5
+wal_or_sam_per = single_per * .9
+wal_or_sam_tan = tanning * .9
+#Target Corporation
+target_start = startup_1 * .5
+target_per = single_per * .9
+target_tan = tanning * .9
+#Walgreens
+walgreens_start = startup_1 * .5
+walgreens_per = single_per * .9
+#Inner Strength Martial Arts
+inner_strength_per = single_per * .9
+#ClearWater Christian Church
+clearwater_per = single_per * .9
+#Military
+mil_per = single_per * .9
+mil_extra = extra * .9
+mil_start = startup_1 * .5
+mil_extra_start = startup_extra * .5
+#silver sneakers
+silver = single_per * 0
+silver_start = startup_1 * 0
+#Home Depot
+home_depot_per = single_per * .9
+home_depot_start = startup_1 * .5
+#Pepsi
+pepsi_per = single_per * .9
+pepsi_start = startup_1 * .5
+#Student
+student = single_per * .9
+student_fee = startup_1 * .5
+#Coca-Cola
+coca_cola = single_per * .9
+coca_cola_fee = startup_1 * .5
+#Delta
+delta = single_per * .9
+#Kohl's
+kohls = single_per * .9
+kohls_start = startup_1 * .5
+#Unitedhealth Allies
+unitedhealth = single_per * .9
+unitedhealth_start = startup_1 * .5
